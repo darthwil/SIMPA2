aa104cb316a8365e8f4e04e9b95ddbd60cd1cfc1
wilber <ingwilberlc@gmail.com>
2025-10-08 00:39:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,22 +6,23 @@ const maxregistros = 20000;
 const geo_server = `${base_url}/collections/public.${recintos}/items?limit=${maxregistros}`; // direccion del path
 const geo_server_municipios = `${base_url}/collections/public.${municipios}/items?limit=${maxregistros}`; // direccion del path
 const geo_local = 'ue.geojson'; //direccion del local
-//const geo_local='https://raw.githubusercontent.com/geoinnova/Points/master/points.json'
-//const geo_local='uno.json'
+const geo_local_municipios = 'municipios.geojson'; //direccion del local
 
 export const origendatos = geo_local;
-export const origendatos_municipios = geo_server_municipios;
+export const origendatos_municipios = geo_local_municipios;
 
 export const limite = [
-  [-70.50, -23.80],
-  [-53.00, -9.50]
+  [-73.00, -25.00],
+  [-54.00, -8.00]
 ];
 
 export const popupFields = [
-  "des_dis","des_ue","partido1","partido2","partido3","partido4","partido5",
+  "des_dis","des_ue"
+];
+export const popupDatos = [
+  "partido1","partido2","partido3","partido4","partido5",
   "partido6","partido7","partido8","partido9","votosnulos","actasnulas","actasobservadas"
 ];
-
 export const tableFields = [
   "des_dis","des_ue","partido1","partido2","partido3","partido4","partido5",
   "partido6","partido7","partido8","partido9","votosnulos","actasnulas","actasobservadas"
@@ -32,4 +33,5 @@ export const sumFields = [
   "partido1","partido2","partido3","partido4","partido5",
   "partido6","partido7","partido8","partido9","votosnulos","actasnulas","actasobservadas"
 ];
+
 
